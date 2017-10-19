4dcb40225d90c4c2c8de0ab091b6b9887637c598
Kia Choi <sgchoi529@email.arizona.edu>
2017-10-19 12:26:44 -0700

    Kia's Commit - Still Errors Running

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,10 +20,13 @@ class TabLink {
   constructor(element, parent) {
     this.element = element;// attach dom element to object
     this.tabs = parent;// attach parent to object
-    this.tabItem = getTab(this.tabItem);// assign this to the associated tab using the 
+    this.tabItem = parent.getTab(this.element.dataset.tab);
+    console.log(this.tabItem);
+    // assign this to the associated tab using the 
     // parent's "getTab" method by passing it the correct data
     // reassign this.tabItem to be a new instance of TabItem, passing it this.tabItem
     this.element.addEventListener('click', () => {
+      console.log(this.tabItem);
       this.tabs.updateActive(this);
       this.select();
     });
@@ -33,19 +36,21 @@ class TabLink {
     // select this link
     // select the associated tab
     this.element.classList.add("Tabs__link-selected");
+    this.tabItem.select();
   }
 
   deselect() {
     // deselect this link
     // deselect the associated tab
     this.element.classList.remove("Tabs__link-selected");
+    this.tabItem.deselect();
   }
 }
 
 class Tabs {
   constructor(element) {
     this.element = element;// attaches the dom node to the object as "this.element"
-    this.links = element.querySelectorAll(".Tabs__link");
+    this.links = this.element.querySelectorAll(".Tabs__link");
     this.links = Array.from(this.links).map((link) => {
       return new TabLink(link, this);
     });
@@ -54,17 +59,20 @@ class Tabs {
   }
 
   init() {
-    // select the first link and tab upon ititialization
-    
+    // select the first link and tab upon initialization
+    this.activeLink.select();
   }
 
   updateActive(newActive) {
     // deselect the old active link
     // assign the new active link
+    this.activeLink.deselect();
+    this.activeLink = newActive;
   }
 
   getTab(data) {
     // use the tab item classname and the data attribute to select the proper tab
+    return this.element.querySelector(`.Tabs__item [data-tab=${data}]`);
   }
 
 }
