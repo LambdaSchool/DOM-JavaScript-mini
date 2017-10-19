fdf37e6f83c336246596d03460cc935595e162ff
Jeff <admin@etisdew.website>
2017-10-18 21:36:58 -0700

    Update Tabs.js
    I wasn't sure if it'd be appropriate to label any as a get or set.
    Quick Test:
    ```// let tabs = document.querySelectorAll(".Tabs");
    // const MappedTabs = Array.from(tabs).map(tabs => new Tabs(tabs));
    // console.log(MappedTabs);
    ```

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,66 +1,32 @@
-
 class TabItem {
-  constructor(element) {
-    // attach dom element to object. Example in Tabs class
-  }
-
-  select() {
-    // should use classList
-  }
-
-  deselect() {
-    // should use classList
-  }
+  constructor(element, classChildSelector = "Tabs__item-selected") {this.element = element; this.classSelector = classChildSelector}
+  select() {this.element.classList.add(this.classSelector)}
+  deselect() {this.element.classList.remove(this.classSelector)}
 }
 
 class TabLink {
-  constructor(element, parent) {
-    this.element;// attach dom element to object
-    this.tabs;// attach parent to object
-    this.tabItem;// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
-    // reassign this.tabItem to be a new instance of TabItem, passing it this.tabItem
-    this.element.addEventListener('click', () => {
-      this.tabs.updateActive(this);
-      this.select();
-    });
-  };
-
-  select() {
-    // select this link
-    // select the associated tab
+  constructor(element, parent, classSelector = "Tabs__link-selected") {
+    this.element = element;
+    this.tabs = parent;
+    this.classSelector = classSelector;
+    this.tabItem = new TabItem(this.tabs.getTab(this.element.dataset.tab));
+    this.element.addEventListener('click', () => {this.tabs.updateActive(this); this.select()});
   }
-
-  deselect() {
-    // deselect this link
-    // deselect the associated tab
-  }
+  select() {this.tabItem.select(); this.element.classList.add(this.classSelector)}
+  deselect() {this.tabItem.deselect(); this.element.classList.remove(this.classSelector)}
 }
 
 class Tabs {
   constructor(element) {
-    this.element = element;// attaches the dom node to the object as "this.element"
-    this.links = element.querySelectorAll(".Tabs__link");
-    this.links = Array.from(this.links).map((link) => {
-      return new TabLink(link, this);
-    });
+    this.element = element;
+    this.links = Array.from(element.querySelectorAll(".Tabs__link")).map((link) => new TabLink(link, this));
     this.activeLink = this.links[0];
     this.init();
   }
-
-  init() {
-    // select the first link and tab upon ititialization
-  }
-
   updateActive(newActive) {
-    // deselect the old active link
-    // assign the new active link
+    this.activeLink.deselect();
+    this.activeLink = newActive;
   }
-
-  getTab(data) {
-    // use the tab item classname and the data attribute to select the proper tab
-  }
-
+  getTab(data) {return this.element.querySelector(`.Tabs__item[data-tab="${data}"]`)}
+  init() {this.activeLink.select()}
 }
-
-let tabs = document.querySelectorAll(".Tabs");
-tabs = Array.from(tabs).map(tabs => new Tabs(tabs));
