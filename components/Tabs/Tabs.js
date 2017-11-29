60a8b37f22aa41fae97befa82ad750bdb33dc4d8
ubuntu <irishseanvaldivia@gmail.com>
2017-11-29 15:53:09 -0600

    Now it works.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,6 +21,7 @@ class TabLink {
     this.element = element;// attach dom element to object
     this.tabs = parent;// attach parent to object
     this.tabItem = parent.getTab(this.element.dataset.tab);// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.tabItem = new TabItem(this.tabItem);
     // reassign this.tabItem to be a new instance of TabItem, passing it this.tabItem
     this.element.addEventListener('click', () => {
       this.tabs.updateActive(this);
@@ -39,14 +40,14 @@ class TabLink {
     // deselect this link
     // deselect the associated tab
     this.element.classList.remove('Tabs__link-selected');
-    this.tabItem.deselect();
+    this.tabItem.deselect()
   }
 }
 
 class Tabs {
   constructor(element) {
     this.element = element;// attaches the dom node to the object as "this.element"
-    this.links = element.querySelectorAll(".Tabs__link");
+    this.links = element.querySelectorAll(".Tabs__link"); // grabs all the tab links
     this.links = Array.from(this.links).map((link) => {
       return new TabLink(link, this);
     });
@@ -55,7 +56,7 @@ class Tabs {
   }
 
   init() {
-    // select the first link and tab upon ititialization
+    // select the first link and tab upon initialization
     this.activeLink.select();
   }
 
