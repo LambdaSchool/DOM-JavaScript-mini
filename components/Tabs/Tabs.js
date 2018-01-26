4658a68437657398e51e2ce97f31998e0927052d
Lo Saephan <teamsky916@gmail.com>
2018-01-25 18:12:37 -0800

    factory function

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,85 +1,41 @@
-class TabItem {
-  constructor(element) {
-    // attach dom element to object. Example in Tabs class
-    this.element = element;
-  }
-  select() {
-    // should use classList
-    this.element.classList.add('Tabs__item-selected');
-  }
-
-  deselect() {
-    // should use classList
-    this.element.classList.remove('Tabs__item-selected');
-  }
-}
-
-class TabLink {
-  constructor(element) {
-    this.element = element; // attach dom element to object
-  };
-
-  select() {
-    // select this link
-    this.element.classList.add('Tabs__link-selected');  
-  }
-
-  deselect() {
-    // deselect this link
-    this.element.classList.remove('Tabs__link-selected');
-  }
-}
-
-class Tabs {
-  constructor(element) {
-    this.element = element;// attaches the dom node to the object as "this.element"
-    this.element.addEventListener('click', (event) => {
-      this.updateActive(event.target.dataset.tab);
-      event.stopPropagation();
-    });
-    // retrieve link nodes
-    this.links = element.querySelectorAll(".Tabs__link");
-    //** return an array of link object
-    this.links = Array.from(this.links).map((link) => {
-      return new TabLink(link, this);
-    });
-
-    //** retrieve all tabs item nodes
-    this.tabItems = element.querySelectorAll(".Tabs__item");
-    //** return an array of tab items
-    this.tabItems = Array.from(this.tabItems).map((item) => {
-      return new TabItem(item);
-    });
-
-    //** assign the first link and tab item in the array to be active / for tracking
-    this.activeLink = this.links[0];
-    this.activeItem = this.tabItems[0];
-    //** initiate the first active link and tab
-    this.init();
-  }
-
-  init() {
-    // select the first link and tab item to be active
-    this.links[0].element.classList.add('Tabs__link-selected');
-    this.tabItems[0].element.classList.add('Tabs__item-selected');
-  }
-
-  updateActive(index) {
-    // update active link
-    if(index){
-      this.activeLink.deselect();
-      this.links[index-1].select();
-      this.activeLink = this.links[index-1];
-      // update active tab item
-      this.activeItem.deselect();
-      this.tabItems[index-1].select();
-      this.activeItem = this.tabItems[index-1];
-      }
-  }
-}
-
 let tabs = document.querySelectorAll(".Tabs");
-
-Array.from(tabs).forEach((tabs) => {
-  new Tabs(tabs)
-});
+// factory function
+const Tabs = function (element) {
+  // retrieve links and items node list
+  const links = element.querySelectorAll('.Tabs__link');
+  const items = element.querySelectorAll('.Tabs__item');
+  // add click event to parent node element
+  element.addEventListener('click', (event) => {
+    // on click event, update the active link and item
+    tabs.updateActive(event.target.dataset.tab);
+    event.stopPropagation();
+  });
+  // make first link and item active on page load
+  links[0].classList.add('Tabs__link-selected');
+  items[0].classList.add('Tabs__item-selected');
+  // return tabs object
+  return {
+    // object properties
+    element: element,
+    links: links,
+    items: items,
+    activeLink: links[0],
+    activeItem: items[0],
+    // handle updating the active link and item
+    updateActive: (index) => {
+      if(index){
+      // remove class from active link and item
+      tabs.activeLink.classList.remove('Tabs__link-selected');
+      tabs.activeItem.classList.remove('Tabs__item-selected');
+      // add class to active link and item
+      tabs.links[index-1].classList.add('Tabs__link-selected');
+      tabs.items[index-1].classList.add('Tabs__item-selected');
+      // re-assign active link and item
+      tabs.activeLink = tabs.links[index-1];
+      tabs.activeItem = tabs.items[index-1];
+      }
+    }
+  }
+}
+// create tabs object and passing in first node
+tabs = Tabs(tabs[0]);
