8ff480273f915d4d341e5d94fa46a788563d99e5
Dani Tacheny <dani.tacheny@gmail.com>
2018-01-25 19:20:30 -0600

    close enough?

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,55 +14,69 @@ class TabItem {
 }
 
 class TabLink {
-  constructor(element, parent) {
+  constructor(element) {
     this.element = element;// attach dom element to object
-    this.tabs = parent;// attach parent to object
-    
-    this.tabItem = this.tabs.getTab(this.element.dataset.tab);
-    // assign this to the associated tab using the parent's "getTab" method by passing it the correct data
-    // reassign this.tabItem to be a new instance of TabItem, passing it this.tabItem
-
-    this.tabItem = new TabItem(this.tabItem);    
-    this.element.addEventListener('click', () => {
-      this.tabs.updateActive(this);
-      this.select();
-    });
-  };
+  }
 
   select() {
     this.element.classList.add("Tabs__link-selected");// select this link
-    this.tabItem.select();// select the associated tab
   }
 
   deselect() {
     this.element.classList.remove("Tabs__link-selected");// deselect this link
-    this.tabItem.deselect();// deselect the associated tab
   }
 }
+
+// "Try to leverage the fact that the link and the item share some data,
+// and make the link ignorant of the item."
 
 class Tabs {
   constructor(element) {
     this.element = element;// attaches the dom node to the object as "this.element"
     this.links = element.querySelectorAll(".Tabs__link");
-    this.links = Array.from(this.links).map((link) => {
-      return new TabLink(link, this);
+    this.links = Array.from(this.links).map(link => {
+      return new TabLink(link);
     });
+    this.items = element.querySelectorAll(".Tabs__item");
+    this.items = Array.from(this.items).map(item => {
+      return new TabItem(item);
+    });
+    document.addEventListener("click", () => {
+      if (event.target.dataset.tab) {
+        const data = event.target.dataset.tab;
+        let tabLink = this.element.querySelector(`.Tabs__link[data-tab="${data}"]`);
+        tabLink = new TabLink(tabLink);
+        let tabItem = this.element.querySelector(`.Tabs__item[data-tab="${data}"]`);
+        tabItem = new TabItem(tabItem);
+        this.updateActive(tabLink, tabItem);
+        tabLink.select();
+        tabItem.select();
+      }
+    });
+
     this.activeLink = this.links[0];
+    this.activeItem = this.items[0];
     this.init();
   }
 
   init() {
     this.activeLink.select();// select the first link and tab upon ititialization
+    this.activeItem.select();
   }
 
-  updateActive(newActive) {
+  updateActive(newActiveLink, newActiveItem) {
+    //console.log(newActiveLink, newActiveItem);
     this.activeLink.deselect();// deselect the old active link
-    this.activeLink = newActive;// assign the new active link
+    this.activeItem.deselect();
+    this.activeLink = newActiveLink;// assign the new active link
+    this.activeItem = newActiveItem;
     this.activeLink.select();
+    this.activeItem.select();
   }
 
   getTab(data) {
-    return this.element.querySelector(`.Tabs__item[data-tab="${data}"]`);
+    return this.element.querySelector(`.Tabs__link[data-tab="${data}"]`);
+    // use the tab item classname and the data attribute to select the proper tab
   }
 }
 
